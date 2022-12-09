7edf0a7f2749898ed97ddf1f8dc70e659a302ea7
msrdeepu <109930907+msrdeepu@users.noreply.github.com>
2022-12-09 12:40:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Board from "./components/Board";
+import "./styles/root.style.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
